bceb87658dad4609bda238ce47d4c72c537c5444
Artem Vasiliev <artvasdmi@gmail.com>
2022-07-07 11:47:42 +0200

    Add RepoController

---
+++ PhoneInfoMVC/Controllers/TellInfoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhoneInfoMVC.Models;
+
+namespace PhoneInfoMVC.Controllers
+{
+    public class TellInfoController : Controller
+    {
+        private ITellInfoRepository repository;
+
+        public TellInfoController(ITellInfoRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult List() => View(repository.TellInfo);
+    }
+}
